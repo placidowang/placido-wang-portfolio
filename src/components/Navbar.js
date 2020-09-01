4f233dece6eb7dd7c712282449d2213bb0d10ab5
placidowang <placidow@gmail.com>
2020-08-31 21:19:21 -0500

    added color palette, script to create redirects in build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,36 +3,46 @@ import { NavLink } from 'react-router-dom';
 
 const activePageLink = {
   backgroundColor: 'white',
-  color: 'purple',
-  // fontWeight: 'bold',
-  underline: 'none'
+  color: '#5bc0be',
+  fontWeight: 'bold',
+  underline: 'none',
+  // width: '100%',
 }
 
 const Navbar = () => <div className='navbar'>
   <div className='navbar-name'>
     <h1>Placido Wang</h1>
+    <p>Junior Software Engineer</p>
   </div>
   <div className='navlink-wrapper'>
+    <div>
     <NavLink to='/' exact
       className='navlink'
       activeStyle={activePageLink}>
       Home
     </NavLink>
+    </div>
+    <div>
     <NavLink to='/projects' exact
       className='navlink'
       activeStyle={activePageLink}>
       Projects
     </NavLink>
+    </div>
+    <div>
     <NavLink to='/about' exact
       className='navlink'
       activeStyle={activePageLink}>
       About
     </NavLink>
+    </div>
+    <div>
     <NavLink to='/contact' exact
       className='navlink'
       activeStyle={activePageLink}>
       Contact
     </NavLink>
+    </div>
   </div>
 </div>
 
