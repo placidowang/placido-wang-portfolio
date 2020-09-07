06799d6f2b2c699480729dbe09410c2890bda7c7
placidowang <placidow@gmail.com>
2020-09-07 15:03:44 -0500

    started projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Projects = () => <div className='App-main projects'>
-  <h1>Projects</h1>
+  <h1 className='App-main-title'>Projects</h1>
+  <div className='project-1' >
+    <div className='video-wrapper'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/nPe_mdcDcUk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+    </div>
+    <h1>Coup React</h1>
+  </div>
 </div>
 
 export default Projects;
