f84e45380c3fa00f78f00d1befe587a914ada7b5
placidowang <placidow@gmail.com>
2020-09-07 16:56:39 -0500

    proj cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import leagueGif from '../assets/project-2/league-tracker-demo.gif';
 
 const Projects = () => <div className='App-main projects'>
   <h1 className='App-main-title'>Projects</h1>
-  <div className='project-1' >
-    <div className='video-wrapper'>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/nPe_mdcDcUk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true'></iframe>
+  <div className='project-card project-2' >
+      <div className='project-thumb'>
+        <img src={leagueGif}/>
+      </div>
+      <div className='project-description'>
+        <h1>League Tracker</h1>
+        <p>League tracker app that dadadadada dfjalkfdj sa;jdkslfjldkasjflkdsajlfkd</p>
+        <p>built with whatever</p>
+      </div>
+    </div>    <div className='project-card project-2' >
+      <div className='project-thumb'>
+        <img src={leagueGif}/>
+      </div>
+      <div className='project-description'>
+        <h1>League Tracker</h1>
+        <p>League tracker app that dadadadada dfjalkfdj sa;jdkslfjldkasjflkdsajlfkd</p>
+        <p>built with whatever</p>
+      </div>
     </div>
-    <h1>Coup React</h1>
-  </div>
+    <div className='project-card project-2' >
+      <div className='project-thumb'>
+        <img src={leagueGif}/>
+      </div>
+      <div className='project-description'>
+        <h1>League Tracker</h1>
+        <p>League tracker app that dadadadada dfjalkfdj sa;jdkslfjldkasjflkdsajlfkd</p>
+        <p>built with whatever</p>
+      </div>
+    </div>
 </div>
 
 export default Projects;
