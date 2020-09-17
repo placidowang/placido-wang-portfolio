4f12088cc3d29d0c1820f6054aacca6b84c6e17e
placidowang <placidow@gmail.com>
2020-09-17 02:06:26 -0500

    clicking outside of burger menu now closes it

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useOnClickOutside } from '../hooks.js';
 import Name from '../components/Name.js';
 import NavlinksComponent from '../components/NavlinksComponent.js'
 import MediaLinksComponent from '../components/MediaLinksComponent.js'
@@ -6,23 +7,31 @@ import Burger from '../components/Burger.js';
 import Menu from '../components/Menu.js';
 import './Navbar.css';
 
-const Navbar = ({open, setOpen}) => <div className='navbar'>
-  <div className='navbar-left'>
-    <Name />
-  </div>
-  <div className='navbar-right'>
-    <NavlinksComponent />
-    <MediaLinksComponent />
-  </div>
 
-  <div className='burger-menu'>
-    <Burger
-      open={open}
-      setOpen={setOpen} />
-    <Menu
-      open={open}
-      setOpen={setOpen} />
-  </div>
-</div>
+const Navbar = ({open, setOpen}) => {
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
+
+  return (
+    <div className='navbar'>
+      <div className='navbar-left'>
+        <Name />
+      </div>
+      <div className='navbar-right'>
+        <NavlinksComponent />
+        <MediaLinksComponent />
+      </div>
+
+      <div className='burger-menu' ref={node}>
+        <Burger
+          open={open}
+          setOpen={setOpen} />
+        <Menu
+          open={open}
+          setOpen={setOpen} />
+      </div>
+    </div>
+  )
+};
 
 export default Navbar;
