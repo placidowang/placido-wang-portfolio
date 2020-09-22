2961edfa9c8ba731b73a8a0da9340a3f6af6d9e4
placidowang <placidow@gmail.com>
2020-09-22 03:45:48 -0500

    adding success message on contact form submit, fixing styling for validations

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,48 +1,65 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import './Contact.css';
-import{ init } from 'emailjs-com';
+import {init, sendForm } from 'emailjs-com';
 init("user_10fjvwDAhJO2tzjJ3NOfh");
 
 const Contact = () => {
   const { register, handleSubmit, watch, errors } = useForm();
+  const contactNumber = Math.random() * 1000000 | 0;
+
   const onSubmit = data => {
-    console.log(data)
+    const form = document.querySelector('#contact-form')
+    const successMsg = document.querySelector('.success-message')
+
+    sendForm('default_service', 'template_cv55kbl', 'contact-form')
+      .then(r => console.log('SUCCESS!', r.status, r.text), error => console.log('FAILED...', error));
+    form.reset();
+    
+    successMsg.className = 'success-message sent';
+    setTimeout(()=> {
+      successMsg.className = 'success-message'
+    }, 4000)
   };
 
+
   const name = watch('name') || "";
+  // eslint-disable-next-line
   const nameCharsLeft = 30 - name.length;
 
   const message = watch('message') || "";
-  const messageCharsLeft = 500 - message.length;
+  const messageCharsLeft = 1000 - message.length;
 
   return (
     <div className="App-main contact">
       <h1 className='App-main-title'>Contact</h1>
-      <form className='email-form' onSubmit={handleSubmit(onSubmit)}>
-        <div className='email-form-fields'>
-          <input type='hidden' name='contact_number'/>
+      <p className='success-message'>Message sent!</p>
+      <form className='contact-form' id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+        <div className='contact-form-fields'>
+          <input type='hidden' name='contact_number' value={contactNumber} />
 
-          {errors.name && errors.name.type === "required" && (
-            <div><span role="alert">Name is required</span><br/></div>
+          {errors.user_name && errors.user_name.type === "required" && (
+            <div role="alert">Name is required<br/></div>
           )}
-          <input type='text' name='name' placeholder='Name' 
+          <input type='text' name='user_name' placeholder='Name' 
           maxLength='30'
-          aria-invalid={errors.name ? "true" : "false"}
+          aria-invalid={errors.user_name ? "true" : "false"}
           ref={register({ required: true })}/>
           {/* <p className='chars-left'>{nameCharsLeft}</p> */}
+          <br/>
 
-          {errors.email && errors.email.type === "required" && (
-            <div><span role="alert">Email is required</span><br/></div>
+        
+          {errors.user_email && errors.user_email.type === "required" && (
+            <div role="alert">Email is required<br/></div>
           )}
-          <input type='email' name='email' placeholder='Email' aria-invalid={errors.email ? "true" : "false"}
+          <input type='email' name='user_email' placeholder='Email' aria-invalid={errors.user_email ? "true" : "false"}
           ref={register({ required: true })}/><br/>
 
 
           {errors.message && errors.message.type === "required" && (
-            <div><span role="alert">Message is required</span><br/></div>
+            <div role="alert">Message is required<br/></div>
           )}
-          <textarea type='text' name='message' placeholder='Message' maxLength='500'
+          <textarea type='text' name='message' placeholder='Message' maxLength='1000'
           aria-invalid={errors.message ? "true" : "false"}
           ref={register({ required: true })}/>
           <p className='chars-left'>{messageCharsLeft}</p>
@@ -51,7 +68,6 @@ const Contact = () => {
         <br/>
         <input type='submit' className='submit-btn' value='Send' />
       </form>
-      {/* <a href='https://www.freepik.com/vectors/background' style={{color: 'white'}}>Background vector created by pikisuperstar - www.freepik.com</a> */}
     </div>
   )
 }
