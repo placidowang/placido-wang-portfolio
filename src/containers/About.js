baf11db30e9ad5953c732735db6130783b7210ae
placidowang <placidow@gmail.com>
2020-09-28 23:50:56 -0500

    Adding to About page

--- src/containers/About.js
+++ src/containers/About.js
@@ -9,8 +9,12 @@ const About = () => <div className="App-main about">
     <h3>And I'm an engineer.</h3>
   </div>
   <p>My dad told me when I was four years old, I touched a computer mouse for the first time and started drawing in MS Paint.</p>
-  <p>He knew from that point that I would be an engineer.</p>
-  <p></p>
+  <p>He saw how quickly I started using something I'd never used before, and I was using it to express myself through a technological tool.</p>
+  <p>He saw how quickly I understood how to use this technological tool for my own expression.</p>
+  <p>He saw curiosity, creativity, and intuition.</p>
+  <p>From this point, he knew I would become an engineer.</p>
+  <br/>
+  <p>I later learned how to build my own computers to fuel my video game <span style={{textDecoration: 'line-through'}}>obsession</span> hobby.</p>
 </div>
 
 export default About;
