1dad48373a64fabdfba2ff3bf5f75dfb567d055c
placidowang <placidow@gmail.com>
2020-09-28 23:36:12 -0500

    Starting About page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './About.css';
 
 const About = () => <div className="App-main about">
   <h1 className='App-main-title'>About</h1>
+  <div className='about-hero'>
+    <h1>Hi</h1>
+    <h2>I'm Placido Wang</h2>
+    <h3>And I'm an engineer.</h3>
+  </div>
+  <p>My dad told me when I was four years old, I touched a computer mouse for the first time and started drawing in MS Paint.</p>
+  <p>He knew from that point that I would be an engineer.</p>
+  <p></p>
 </div>
 
 export default About;
