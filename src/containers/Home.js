7a536a4072880b3f2db8b1e70c1b8aa867f6c410
placidowang <placidow@gmail.com>
2020-09-30 23:57:01 -0500

    adding to home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Home.css';
+import coupDemoGif from '../assets/projects/coup-react-demo.gif';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => <main className='App-main home'>
   {/* <h1>Home</h1> */}
   <header className='home-header'>
     <div className='home-header-text'>
-      <h1>Coming soon!</h1>
-      {/* <h1 className='my-name'>Placido Wang</h1> */}
+      {/* <h1>Coming soon!</h1> */}
+      <h1 className='my-name'>Placido Wang</h1>
       {/* <p>Junior Software Engineer</p> */}
     </div>
   </header>
@@ -15,18 +18,23 @@ const Home = () => <main className='App-main home'>
     <h5>-Louis Srygley</h5>
   </div><br/>
   <div className='placeholder-text'>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vulputate lobortis felis, at tincidunt augue mollis vitae. Pellentesque lorem leo, tincidunt vitae auctor id, pretium dignissim erat. Nulla facilisi. Duis ac leo leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi vitae lacinia tellus. Nam pretium nunc orci, ac varius tortor bibendum vitae. Fusce in auctor est. Curabitur ornare iaculis orci, id consequat est suscipit id. Vivamus a odio sodales, ultricies urna ac, dapibus leo. Nam eget congue augue. Ut pretium urna eget augue suscipit volutpat. Sed non dolor velit. Morbi in dictum sapien.</p><br/>
-<p>
-Donec quis fermentum arcu. Proin a ornare sapien. Vestibulum commodo ante et sem placerat aliquet. Pellentesque eu viverra est. Nulla ut semper erat. Fusce et finibus velit. Aliquam at dignissim purus. Ut in est urna. Nam dapibus enim erat, sed luctus augue rutrum sit amet. Vivamus posuere ac nibh ut rutrum. Ut egestas, turpis sed blandit blandit, sem ex fringilla odio, quis pulvinar enim justo ut lectus. Sed tincidunt leo ac lacus pulvinar, a convallis enim malesuada. Mauris felis lorem, convallis a sem in, fringilla ultrices nunc.</p><br/>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vulputate lobortis felis, at tincidunt augue mollis vitae. Pellentesque lorem leo, tincidunt vitae auctor id, pretium dignissim erat. Nulla facilisi. Duis ac leo leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi vitae lacinia tellus. Nam pretium nunc orci, ac varius tortor bibendum vitae. Fusce in auctor est. Curabitur ornare iaculis orci, id consequat est suscipit id. Vivamus a odio sodales, ultricies urna ac, dapibus leo. Nam eget congue augue. Ut pretium urna eget augue suscipit volutpat. Sed non dolor velit. Morbi in dictum sapien.</p>
+  </div>
+  <br/>
+  <a href='projects'><h3>Check out my projects!</h3></a>
+  <div className='project-slideshow'>
+    <a href='projects'>
+      <img className='demo' alt='coup thumb' src={coupDemoGif} />
+    </a>
+  </div>
+  <br/>
+  <a className='contact-me' href='contact'><FontAwesomeIcon icon={faEnvelopeSquare} size='2x'/><h3>Contact me!</h3></a>
+  
+
 <p>
 Aenean eget tortor vitae nisi ultricies ultricies. Suspendisse laoreet fermentum mauris, at venenatis purus sodales quis. Nullam iaculis vitae lacus ac ultricies. In aliquam, ante dictum imperdiet porta, magna ipsum auctor massa, in dapibus ipsum purus ac magna. Phasellus elementum suscipit nulla eget efficitur. In hendrerit erat eu lectus facilisis euismod. Maecenas lobortis nunc urna, eget condimentum dui sollicitudin tempor. Sed sed vestibulum justo.
 </p><br/>
-<p>
-Sed porttitor tincidunt libero, nec gravida erat convallis eu. Phasellus a felis elit. Mauris sagittis lacus id scelerisque ultricies. Nunc sollicitudin scelerisque varius. Nunc sit amet facilisis nibh. Etiam sit amet consectetur mi. Nunc eleifend lectus quis tellus bibendum, nec maximus tellus auctor. Suspendisse in egestas purus. Etiam luctus ac tellus in tempus. Nullam non laoreet arcu. In ornare, massa quis vehicula pretium, eros eros commodo nunc, id bibendum odio leo ac turpis.
-</p><br/>
-<p>
-Etiam id cursus libero. Fusce fermentum felis id rhoncus malesuada. Pellentesque a blandit ante. Nulla egestas efficitur sem, vel vestibulum odio mollis et. Morbi non orci non arcu elementum rutrum. Praesent vitae laoreet nisi. Duis in ex a turpis tristique condimentum vel vitae neque. Cras sem odio, sollicitudin in aliquet vitae, vehicula id lacus. Sed mattis dolor ut arcu cursus, nec rutrum justo interdum. Nulla at convallis ante, nec pharetra ligula. Integer ornare diam viverra orci eleifend vehicula. Pellentesque imperdiet augue vel nisi gravida tincidunt. Suspendisse potenti. Nullam libero quam, sodales sit amet consequat feugiat, varius sed massa. Praesent consectetur at arcu quis bibendum.</p>
-  </div>
+
 </main>
 
 export default Home;
