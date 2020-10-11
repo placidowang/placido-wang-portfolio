6533087515f93fdc84373dfa045f7da27d16c780
placidowang <placidow@gmail.com>
2020-10-11 16:14:55 -0500

    reconfiguring styling to allow for 'padding' in app main elements

--- src/components/Name.js
+++ src/components/Name.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
   faPlusCircle
-} from '@fortawesome/free-solid-svg-icons'
+} from '@fortawesome/free-solid-svg-icons';
+import portrait from '../assets/my_portrait.png';
 
 const Name = () => 
   <a href='/'>
     <div className='navbar-name'>
-      <FontAwesomeIcon icon={faPlusCircle} size='lg' />
+      {/* <FontAwesomeIcon icon={faPlusCircle} size='lg' /> */}
+      <img className='navbar-portrait' src={portrait} />
       <h1>Placido Wang</h1>
       {/* <p>Software Engineer</p> */}
     </div>
